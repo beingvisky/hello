a74988c95094a58b9cc640b49c461456377b703c
Vishaha Ram S <vishak.28@gmail.com>
2020-12-21 15:20:57 +0530

    hello

---
+++ hello/src/main/java/com/lti/hello.java
@@ -0,0 +1,16 @@
+package com.lti;
+
+
+
+public class hello {
+	public static int add(int num1,int num2) {
+		return num1+num2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sum="+add(10,20));
+		//System.out.println("Difference="+sub(90,20));
+	}
+
+}
